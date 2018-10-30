f754ae39e9154c7a4754beea456a6702791a982a
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-10-30 14:10:13 -0300

    making the corrects routs, and actions in the controller to display on the view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  def create
+    @tasks = "?"
+  end
+
+  def read
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    # @taks.?
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @tasks = Task.new(name: "trabalho")
+  end
+
+  def edit
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  # Would substitute all the code above
+  # resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :tasks
+
+  get    "tasks",          to: "tasks#index"
+
+  get    "tasks/new",      to: "tasks#new"
+  post   "tasks",          to: "tasks#create"
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "tasks/:id",      to: "tasks#show"
+
+  get    "tasks/:id/edit", to: "tasks#edit"
+  patch  "tasks/:id",      to: "tasks#update"
+
+  delete "tasks/:id",      to: "tasks#destroy"
 end
