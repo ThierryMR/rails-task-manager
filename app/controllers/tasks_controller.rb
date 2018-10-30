55728d0c8f571e8cdd4b09fa1e229247567bcb6c
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-10-30 14:30:25 -0300

    Some updates at the actions and preventing user missbehavior, but steal an form was not displayed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   def create
-    @tasks = "?"
+    @tasks = Task.find(params[:id])
+    @tasks.save
   end
 
   def read
@@ -8,12 +9,18 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    # @taks.?
+    # Entendi mais ou menos esse params :tasks, pedir por orientacao
+    @tasks.update(params[:tasks])
+
+    @tasks.update(user_params)
+
+    redirect_to tasks_path(@tasks)
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+    redirect_to tasks_path
   end
 
   def show
@@ -25,10 +32,19 @@ class TasksController < ApplicationController
   end
 
   def new
-    @tasks = Task.new(name: "trabalho")
+    @tasks = Task.new
   end
 
   def edit
+    @task = Task.find(params[:id])
 
+  end
+
+    private
+
+  def user_params
+    params.require(:user).permit(:email)
+      # Never trust user data!
+    # params.require(:restaurant).permit(:name, :address)
   end
 end
